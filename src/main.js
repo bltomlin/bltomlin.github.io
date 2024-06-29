e28e929a9e6f232004b2d7ea6f58beef3db84785
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2024-06-28 23:21:04 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -136,7 +136,7 @@ k.scene("main", async () => {
       player.direction = "left";
       return;
     }
-
+  });
 
   function stopAnims() {
     if (player.direction === "down") {
@@ -147,15 +147,62 @@ k.scene("main", async () => {
       player.play("idle-up");
       return;
     }
-      player.play("idle-side");
+
+    player.play("idle-side");
+  }
+
+  k.onMouseRelease(stopAnims);
+
+  k.onKeyRelease(() => {
+    stopAnims();
+  });
+  k.onKeyDown((key) => {
+    const keyMap = [
+      k.isKeyDown("right"),
+      k.isKeyDown("left"),
+      k.isKeyDown("up"),
+      k.isKeyDown("down"),
+    ];
+
+    let nbOfKeyPressed = 0;
+    for (const key of keyMap) {
+      if (key) {
+        nbOfKeyPressed++;
+      }
     }
-  
-    k.onMouseRelease(stopAnims);
-  
-    k.onKeyRelease(() => {
-      stopAnims();
-    });
-});
+
+    if (nbOfKeyPressed > 1) return;
+
+    if (player.isInDialogue) return;
+    if (keyMap[0]) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+      player.move(player.speed, 0);
+      return;
+    }
+
+    if (keyMap[1]) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+      player.move(-player.speed, 0);
+      return;
+    }
+
+    if (keyMap[2]) {
+      if (player.curAnim() !== "walk-up") player.play("walk-up");
+      player.direction = "up";
+      player.move(0, -player.speed);
+      return;
+    }
+
+    if (keyMap[3]) {
+      if (player.curAnim() !== "walk-down") player.play("walk-down");
+      player.direction = "down";
+      player.move(0, player.speed);
+    }
+  });
 });
 
 k.go("main");
